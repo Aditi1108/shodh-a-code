81beb164f4f4c01061aa945a980d0639aafce1f6
Aditi1108 <17ucc006@lnmiit.ac.in>
2025-09-14 13:43:12 +0530

    package name rename

--- backend/contest/src/main/java/com/shodhacode/ContestApplication.java
+++ backend/contest/src/main/java/com/shodhacode/ContestApplication.java
@@ -1,4 +1,4 @@
-package com.shodhacode.contest;
+package com.shodhacode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/contest/src/test/java/com/shodhacode/ContestApplicationTests.java
+++ backend/contest/src/test/java/com/shodhacode/ContestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.shodhacode.contest;
+package com.shodhacode;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
